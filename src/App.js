e936fb51a39de77f1ac167a4af7443045f4fbdf5
PatrickFlorian-dev <patrickfloriansoftwareengineer@gmail.com>
2021-03-04 15:24:56 -0500

    Testing scaffolding branch commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Boilerplate, adding scaffolding in next commit in seperate branch 
+          Testing scaffolding branch commit 
         </p>
       </header>
     </div>
